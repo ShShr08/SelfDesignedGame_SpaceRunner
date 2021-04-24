579512fa9e9f74e81ee632cd0144d76969c104f1
ShanSharma08 <75727925+ShanSharma08@users.noreply.github.com>
2021-04-24 17:07:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 var spImg
 var ast
+var myEngine
+var ground
+var astpos
+var gImg
+var lG
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -8,22 +13,55 @@ const Body = Matter.Body;
 
 function preload(){
     spImg = loadImage("images/Space.png");
+    gImg = loadImage("images/e.png")
 }
 
 function setup(){
     createCanvas(1536,753);
-    world = Engine.world
+    myEngine = Engine.create();
+    world = myEngine.world
 
-    ast = new Astronaught(300,500,100,200);
+    ground = createSprite(768,740,1536,50);
+    ground.addImage(gImg);
+
+    ast = new Astronaught(300,612,300,300);
+    astpos = ast.body.position;
+
+    lG = new Group;
+
 
 }
 
 function draw(){
     background(spImg);
 
-    if(keyCode ===32){
-        Matter.Body.applyForce(ast,ast.body,{x:0,y:-4})
+    if(keyDown(UP_ARROW)){
+        astpos.y = astpos.y -10
+    } 
+    if(keyDown(DOWN_ARROW)){
+        astpos.y = astpos.y +2.5
     }
-     
+    astpos.y = astpos.y+3
+    
+    if(astpos.y>612){
+        astpos.y = 612
+    }
+
+    if(lG.isTouching(ast)){
+        lG.destroyEach();
+        text("GAME OVER",500,500)
+    }
+
     drawSprites();
+    spawnEnemy();
+    ast.display();
+}
+
+function spawnEnemy(){
+    if(frameCount%120===0){
+        Lazer = createSprite(1550,Math.round(random(10,700)));
+        Lazer.velocityX = -10;
+        lG.add(Lazer);
+        
+    }
 }
